af6b10a437eca82169977d377c6dfd8418c8cf43
SeongbinPark <binni0515@gmail.com>
2022-08-05 22:13:06 +0900

    스프링 핵심 원리 기본편 2-7강

---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,60 @@
+package hello.core.order;
+public class Order {//주문다 끝나고 할인 다 했을 때 만들어지는 객체
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;//원가
+    private int discountPrice;//할인가격
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+    //계산된 가격
+    public int calculaterPrice() {
+        return itemPrice-discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(int discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    @Override
+    public String toString() {//order객체를 출력하면 toString이 출력됨.
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
